2fd807ee87bb0f1e5c4ba5b5110001f19dd67c9e
HanaAboauf <142686198+HanaAboauf@users.noreply.github.com>
2025-07-09 23:37:48 +0300

    Add files via upload

---
+++ oop2/Employee.cs
@@ -0,0 +1,88 @@
+﻿
+using System.Globalization;
+
+namespace oop2
+{
+    using System.Globalization;
+
+    namespace oop2
+    {
+        public class Employee
+        {
+            private int id;
+            private string name;
+            private int salary;
+            private SecurityLevel securityLevel;
+            private Gender gender;
+            private HireDate hireDate;
+
+          
+            public Employee(int id, string name, Gender gender, SecurityLevel securityLevel, int salary, HireDate hireDate)
+            {
+                this.id = id;
+                this.name = name;
+                this.gender = gender;
+                this.securityLevel = securityLevel;
+                this.salary = salary;
+                this.hireDate = hireDate;
+            }
+
+      
+            public Employee()
+            {
+                id = 0;
+                name = "Unknown";
+                gender = Gender.F;
+                securityLevel = SecurityLevel.guest;
+                salary = 0;
+                hireDate = new HireDate(1, 1, 2000);
+            }
+
+    
+            public int GetId() => id;
+            public void SetId(int value) => id = value;
+
+            public string GetName() => name;
+            public void SetName(string value) => name = !string.IsNullOrWhiteSpace(value) ? value : "Unknown";
+
+            public int GetSalary() => salary;
+            public void SetSalary(int value) => salary = value >= 0 ? value : 0;
+
+            public SecurityLevel GetSecurityLevel() => securityLevel;
+            public void SetSecurityLevel(SecurityLevel level) => securityLevel = level;
+
+            public Gender GetGender() => gender;
+            public void SetGender(Gender g) => gender = g;
+
+            public HireDate GetHireDate() => hireDate;
+            public void SetHireDate(HireDate date) => hireDate = date;
+
+          
+            public override string ToString()
+            {
+                return $"ID: {id}\n" +
+                       $"Name: {name}\n" +
+                       $"Gender: {gender}\n" +
+                       $"Security Level: {securityLevel}\n" +
+                       $"Salary: {salary}\n" +
+                       $"Hire Date: {hireDate}";
+            }
+        }
+
+        public enum SecurityLevel
+        {
+            guest,
+            Developer,
+            secretary,
+            DBA,
+            securityOfficer
+        }
+
+        public enum Gender
+        {
+            F,
+            M
+        }
+    }
+
+}
---
+++ oop2/HiringDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2
+{
+    public struct HireDate
+    {
+        public int Day { get; }
+        public int Month { get; }
+        public int Year { get; }
+
+        public HireDate(int day, int month, int year)
+        {
+            Day = (day >= 1 && day <= 31) ? day : 1;
+            Month = (month >= 1 && month <= 12) ? month : 1;
+            Year = (year >= 1900) ? year : 2000;
+        }
+
+        public override string ToString()
+        {
+            return $"{Day:D2}/{Month:D2}/{Year}";
+        }
+    }
+
+
+}
