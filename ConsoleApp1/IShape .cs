8da742c04580b2c0c9070f56da24676bbe1935b5
Hana <hanamarwasadek2004@gmail.com>
2025-07-17 09:35:40 +0300

    make the base interfaces

---
+++ ConsoleApp1/IShape .cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop3
+{
+    internal interface IShape
+    {
+        double Area { get; }
+        string DisplayShapeInfo();
+
+
+    }
+
+    internal interface ICircle : IShape
+    {
+    }
+    internal interface IRectangle : IShape
+    {
+    }
+}
