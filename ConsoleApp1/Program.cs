fd8c6138ec4bde8fc6e5f2c231841a79bbce0889
Hana <hanamarwasadek2004@gmail.com>
2025-07-17 10:48:03 +0300

    solution of question2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ConsoleApp1
+﻿using oop3.question2;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            IAuthenticationService authenticationService = new BasicAuthenticationService();
+
+            Console.WriteLine(authenticationService.AuthenticateUser("admin", "password"));
+
+            Console.WriteLine(authenticationService.AuthenticateUser("admin", "pasword"));
+
+            Console.WriteLine(authenticationService.AuthorizeUser("admin", "Administrator"));
+
+            Console.WriteLine(authenticationService.AuthorizeUser("admin", "user"));
+
         }
     }
 }
---
+++ ConsoleApp1/question2/IAuthenticationService .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop3.question2
+{
+    internal interface IAuthenticationService
+    {
+        bool AuthenticateUser(string userName, string password);
+
+        bool AuthorizeUser(string userName, string role);
+
+
+    }
+
+}
