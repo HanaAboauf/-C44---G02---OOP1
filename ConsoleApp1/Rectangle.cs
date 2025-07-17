058afc6904fad28f2cb15ec79f4bf55acad9874a
Hana <hanamarwasadek2004@gmail.com>
2025-07-17 09:38:21 +0300

    implement IRectangle interface

---
+++ ConsoleApp1/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop3
+{
+    internal class Rectangle: IRectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public double Area => Width * Height;
+        public string DisplayShapeInfo()
+        {
+            return $"Rectangle with width {Width} and height {Height} has an area of {Area}.";
+        }
+    }
+   
+}
